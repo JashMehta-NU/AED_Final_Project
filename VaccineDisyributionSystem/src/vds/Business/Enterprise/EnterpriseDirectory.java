5f483ae03f83b8c36663c93ff4bc00afc7445ca5
JashMehta-NU <mehta.jas@northeastern.edu>
2022-11-29 20:13:57 -0500

    Structural Changes

---
+++ VaccineDisyributionSystem/src/vds/Business/Enterprise/EnterpriseDirectory.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vds.Business.Enterprise;
+
+/**
+ *
+ * @author JASH,JUBIN,AAYUSH
+ */
+public class EnterpriseDirectory {
+    
+}
